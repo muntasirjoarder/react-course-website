ba1f9de8d6534c06aa1dc56b7015cb465a768cdb
Muntasir Joarder <muntasir.joarder@gmail.com>
2018-11-07 14:58:54 +1000

    second commit

---
+++ src/pages/page-2.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const SecondPage = () => (
+  <div>
+    <h1>Learn to design and code</h1>
+    <p>Complete course about the with 12 modules</p>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+)
+
+export default SecondPage
